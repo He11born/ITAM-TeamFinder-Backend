cf0b02b596de1074e22ae95c986af3b78fd63544
He11born <egoosergacev@gmail.com>
2025-12-03 14:29:44 +0300

    Update base.py

--- app/db/base.py
+++ app/db/base.py
@@ -5,4 +5,4 @@ from app.db.session import Base
 # Импортируем все модели, чтобы Alembic их "увидел"
 from app.db.models.user import User, Profile
 from app.db.models.hackathon import Hackathon 
-# from app.db.models.team import Team, TeamMember, Invitation # Эти добавим позже
+from app.db.models.team import Team, TeamMember, Invitation # <-- НОВЫЕ МОДЕЛИ
