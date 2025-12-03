dacd23892f65aa63044656836cf54712f8a08ba8
He11born <egoosergacev@gmail.com>
2025-12-03 14:24:15 +0300

    Update base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,8 +1,8 @@
-# Импортируем базовый класс для всех моделей
+# app/db/base.py
+
 from app.db.session import Base
 
 # Импортируем все модели, чтобы Alembic их "увидел"
-from app.db.models.user import User  # Пока этих файлов нет, мы их создадим далее
+from app.db.models.user import User, Profile
 from app.db.models.hackathon import Hackathon 
-from app.db.models.team import Team
-# ... и другие
+# from app.db.models.team import Team, TeamMember, Invitation # Эти добавим позже
